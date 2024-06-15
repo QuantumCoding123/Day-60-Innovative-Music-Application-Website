e3ad8c5ff3f6dca2dd6a352922024d4a253df77a
Quantum Coding <166281221+QuantumCoding123@users.noreply.github.com>
2024-06-15 11:05:49 +0530

    Add files via upload

---
+++ js/playlist.js
@@ -0,0 +1,38 @@
+let songs = [
+  {
+    name: "Calm Down",
+    artist: "Rema",
+    img: "img01",
+    coverImg: "img01",
+    audio: "calmdown"
+  },
+  {
+    name: "G.O.A.T.",
+    artist: "Diljit Dosanjh",
+    img: "img02",
+    coverImg: "img02",
+    audio: "goat"
+  },
+  {
+    name: "Badshah · Arijit Singh",
+    artist: "Soulmate",
+    img: "img03",
+    coverImg: "img03",
+    audio: "badsh"
+  },
+  {
+    name: "Until I Found You",
+    artist: "Stephen Sanchez",
+    img: "img04",
+    coverImg: "img04",
+    audio: "until"
+  }
+  ,
+  {
+    name: "Give Me Some Sunshine",
+    artist: "Sharman Joshi and Suraj Jagan",
+    img: "img05",
+    coverImg: "img05",
+    audio: "idiot"
+  }
+]
